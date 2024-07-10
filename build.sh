ffeae94f21b6ba449aef9f106b836a07c552a5ed
Luis Correia <buga@loide.net>
2024-07-10 15:09:07 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-WEBSITE_DIR="/pub/provisioning/machinecmd/_data/nginx/sites/labs_oneoverzero_org"
+WEBSITE_DIR="/pub/sites/labs_oneoverzero_org"
 HUGO="../hugo"
 
 $HUGO -d "$WEBSITE_DIR";
